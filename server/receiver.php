d1a421987a980d04be793209372f04b7120beeb5
MatMasIt <mattia.mascarello@outlook.it>
2021-12-08 23:50:01 +0100

    Update receiver module

--- server/receiver.php
+++ server/receiver.php
@@ -1,14 +1,5 @@
 <?php
-$f=file("php://input");
-if($f[0]=="TOKEN"){
-	if(count($f)<2) exit;
-}
-$afs=[];
-for($i=1;$i<count($f);$i++){
-	$beginDay=strtotime(date("Y-m-d",explode(",",$f[$i])[0]));
-	$afs[$beginDay].=$f[$i];
-}
-foreach(array_keys($afs) as $b){
-	file_put_contents("dataServer/".$b.".csv",$afs[$b],FILE_APPEND);
-}
+echo "<h1>Hello!</h1>";
+echo "The direct send service has been phased out, however, you have triggered a data refresh from the git repository<br />";
+include("git.php");
 file_put_contents("lastContact",time());
