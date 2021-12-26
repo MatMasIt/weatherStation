9e917a5b3385a4d47407d9176d6ebb366dcc5d4f
MatMasIt <mattia.mascarello@outlook.it>
2021-12-26 16:05:54 +0100

    Update recent.php

--- server/recent.php
+++ server/recent.php
@@ -1,32 +1,3 @@
 <?php
-
-function lastE($n)
-{
-    $a=explode("/",$n);
-    return $a[count($a)-1];
-}
-$hw=["temperature.csv"=>"T","humidity.csv"=>"H","pressure.csv"=>"P","pm10.csv"=>"PM10","pm25.csv"=>"PM25","smoke.csv"=>"S"];
-$dataL=[];
-$y=0;
-foreach(glob("data/2*") as $yp){
-    $yt=lastE($yp);
-    if($y<$yt) $y=$yt;
-}
-$y=(string) $y;
-$m=0;
-foreach(glob("data/$y/*") as $mp){
-    $mt=lastE($mp);
-    if($m<$mt) $m=$mt;
-}
-$m=(string)$m;
-if(count($m)==0) $m="0".$m;
-$d=0;
-foreach(glob("data/$y/$m/*") as $dp){
-    $dt=lastE($dp);
-    if($d<$dt) $d=$dt;
-}
-foreach($hw as $fn=>$fm){
-    $f=file("data/$y/$m/$d/$fn");
-    $dataL[$fm]=(float)explode(",",$f[count($f)-1])[1];
-}
+require("rlib.php");
 echo json_encode($dataL);
